e2137c34500264940fca23f32b80a12dff164d40
Aram <papoyanaram2@gmail.com>
2023-01-19 00:56:56 +0400

    input

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import './App.css';
 //import User from "./components/User";
 import Product from "./components/Product";
-
+import Input from "./components/input/Input";
 function App() {
   return (
     <div className="App">
+        <Input/>
       {/*<User name={'valod'} age={18} href={'fffff'}/>*/}
       {/*<User name={'aaa'} age={28}/>*/}
         <Product name={'valod'} age={18} price={'1$'}/>
