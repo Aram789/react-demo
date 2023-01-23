b42db35f7d25a8b14b09cef91e70d800d901b331
Aram <papoyanaram2@gmail.com>
2023-01-23 12:10:33 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import ToDo from "./components/todo/ToDo";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
--- src/components/todo/ToDo.js
+++ src/components/todo/ToDo.js
@@ -1,47 +1,64 @@
 import React, {Component} from "react";
+import styles from './todo.module.css';
+import {Button, Col, Row, Container} from 'react-bootstrap';
 
 class ToDo extends Component {
     state = {
         inputValue: '',
-        tasks: []
+        tasks: [],
     }
-    handleChange = (event) =>{
+    handleChange = (event) => {
         this.setState({
             inputValue: event.target.value
         })
     }
-    addTask = () =>{
+    addTask = () => {
         const inputValue = this.state.inputValue.trim();
         const tasks = [...this.state.tasks, inputValue]
-        if(inputValue){
+        if (inputValue) {
             this.setState({
                 tasks: tasks,
-                inputValue: ''
+                inputValue: '',
             })
         }
     }
+    remove = (event) => {
+
+    }
+
     render() {
         const {tasks} = this.state
-        const taskComponents = tasks.map((task, index)=>{
-            return <li key={index}>{task}</li>
+        const taskComponents = tasks.map((task, index) => {
+            return <p className={index===2?styles.selected: ''} key={index}>{task} <Button variant="danger" onClick={this.remove}>X</Button></p>
         })
-        return(
-            <div>
-                <h2>ToDo List</h2>
-                <input
-                    type="text"
-                    onChange={this.handleChange}
-                    value={this.state.inputValue}
-                />
-                <button
-                    onClick={this.addTask}
-
-                >Add Task</button>
-                <ol>
-                    {taskComponents}
-                </ol>
-
-            </div>
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>ToDo List</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="d-flex gap-2">
+                        <input
+                            type="text"
+                            onChange={this.handleChange}
+                            value={this.state.inputValue}
+                        />
+                        <Button
+                            variant="primary"
+                            onClick={this.addTask}
+                        >
+                            Add Task
+                        </Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        {taskComponents}
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
