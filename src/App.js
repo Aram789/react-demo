37782aaa8da0e5fa784c758b9de572c84d4b1958
Aram <papoyanaram2@gmail.com>
2023-02-15 01:42:08 +0400

    ffix

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import './App.css';
 import ToDo from "./components/ToDo/ToDo";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Route, Routes} from "react-router-dom";
 import About from "./components/pages/about/about";
 import Contact from "./components/pages/contact/contact";
 import {NotFound} from "./components/pages/NotFound";
 import NavMenu from "./components/NavMenu/NavMenu";
 import Footer from "./components/Footer";
 import SingleTask from "./components/pages/singleTask/SingleTask";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 //23
 function App() {
     return (
         <div className="App">
-            <NavMenu/>
-            <Routes>
-                <Route path="/"
-                       element={<ToDo/>}
-                />
-                <Route path="/about"
-                       element={<About/>}
-                />
-                <Route path="/contact"
-                       element={<Contact/>}
-                />
-                <Route path="/task/:taskId"
-                       element={<SingleTask/>}
-                />
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
-            <Footer/>
+            <Router>
+                <NavMenu/>
+                <Routes>
+                    <Route path="/"
+                           element={<ToDo/>}
+                    />
+                    <Route path="/about"
+                           element={<About/>}
+                    />
+                    <Route path="/contact"
+                           element={<Contact/>}
+                    />
+                    <Route exact path="/task/:taskId"
+                           element={<SingleTask/>}
+                    />
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+                <Footer/>
+            </Router>
         </div>
     );
 }
--- src/components/pages/singleTask/SingleTask.js
+++ src/components/pages/singleTask/SingleTask.js
@@ -2,8 +2,7 @@ import {Component} from "react";
 
 export default class SingleTask extends Component{
     componentDidMount() {
-        const taskId = this.props
-        console.log(taskId)
+        console.log(this.props)
     }
 
     render() {
@@ -14,3 +13,39 @@ export default class SingleTask extends Component{
         );
     }
 }
+
+
+
+// import * as ReactRouterDOM from "react-router";
+//
+// const useParams = ReactRouterDOM.useParams;
+//
+// export default function SingleTask ()  {
+//     const params = useParams();
+//     fetch('http://localhost:3001/task/'+ params.taskId, {
+//         method: 'GET',
+//         headers: {
+//             "Content-Type": 'application/json'
+//         }
+//     })
+//         .then(async (response) => {
+//             const res = await response.json();
+//             console.log(res)
+//             if (response.status >= 400 && response.status < 600) {
+//                 if (res.error) {
+//                     throw res.error;
+//                 } else {
+//                     throw new Error('Something went wrong!');
+//                 }
+//             }
+//         })
+//         .catch((error) => {
+//             console.log('catch error', error);
+//         });
+//     return (
+//         <div className='container'>
+//             <div>{params.taskId}</div>
+//         </div>
+//     );
+//
+// }
